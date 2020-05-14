49797129b92288a557610bb8f17143b7d1cc92aa
JornMeessen <jornmeessen@hotmail.com>
2020-05-14 14:29:55 +0200

    update opdracht 4

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -1,12 +1,19 @@
 
 
 // changing individual properties with code and using setInterval
-var rotationSpeed = 0.01;
+
+//var rotationSpeed = -0.05;
+var yas = 0.01;
+var xas = -0.05;
+var zas = 0.05;
+var color;
 var myOtherBox = document.getElementById('myOtherBox');
 
 function spin(){
-	myOtherBox.object3D.rotation.x += rotationSpeed;
-	console.log(myOtherBox.object3D.rotation.x);
+	myOtherBox.object3D.rotation.x += xas;
+	myOtherBox.object3D.rotation.y += yas;
+	myOtherBox.object3D.rotation.z += zas;
+	console.log(myOtherBox.object3D.rotation.x, myOtherBox.object3D.rotation.y, myOtherBox.object3D.rotation.z, myOtherBox.object3D.color);
 }
 
 setInterval(spin, 16); //equivalent to 60 fps
